501afbf95f1f2af7ae27340eb8bfa8520c0aca3a
wind <398469205@qq.com>
2015-06-21 21:48:36 +0800

    HTML5开发的App知识总结

---
+++ js/star.js
@@ -0,0 +1,30 @@
+ window.onload=function (){
+	var oStar=document.getElementById("star");
+	var aLi=oStar.getElementsByTagName("li");
+	var oUl=oStar.getElementsByTagName("ul")[0];
+	var oSpan=oStar.getElementsByTagName("span")[1];
+	var oP=oStar.getElementsByTagName("p")[0];
+	var i=iScore=iStar=0;
+	
+	for(i=1;i<=aLi.length;i++){
+		aLi[i-1].index=i;
+		//鼠标移过显示分数
+		aLi[i-1].onmouseover=function(){
+			fnPoint(this.index);
+			oP.style.display="block"//浮动层显示
+			};
+		//鼠标离开后恢复上次评分
+		aLi[i-1].onmouseout=function(){
+			fnPoint();
+			oP.style.display="none"//关闭浮动层
+			};
+		//点击后进行评分处理
+		aLi[i-1].onclick=function(){
+			iStar=this.index;
+			oP.style.display="none"
+			}};
+	//评分处理
+	function fnPoint(iArg){
+		//分数赋值
+		iScore=iArg||iStar;
+		for(i=0;i<aLi.length;i++)aLi[i].className=i<iScore?"on":""}};
